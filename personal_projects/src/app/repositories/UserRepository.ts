773a1b773aad8cfdcffd69e93aace33d30e47465
Sabrina Coelho <80803237+SabrinaCoelho@users.noreply.github.com>
2023-11-08 13:33:03 -0300

    atualizado e funcionando apenas o getUsers

--- personal_projects/src/app/repositories/UserRepository.ts
+++ personal_projects/src/app/repositories/UserRepository.ts
@@ -4,12 +4,25 @@ import { AppDataSource } from "../../database/data-source";
 
 const userRepository = AppDataSource.getRepository(User);
 
-const getUsers = (): Promise<IUser[]> => {
-    return userRepository.find();
+const getUsers = async(_req: any, res: any): Promise<IUser[]> => {
+    //return userRepository.find();
+    const users = await AppDataSource.getRepository(User).find()
+    return res.status(200).json(users);
 }
 
-const postUser = () => {
-    return userRepository.create();
+const postUser = (u: User) => {
+    return userRepository.create(u);
+}
+
+const getUserById = (user: User) => {
+    return userRepository.create(user);
+}
+
+const updateUserById = (user: User) => {
+    return userRepository.create(user);
+}
+const destroyUser = (user: User) => {
+    return userRepository.create(user);
 }
 
 export default { getUsers, postUser };
