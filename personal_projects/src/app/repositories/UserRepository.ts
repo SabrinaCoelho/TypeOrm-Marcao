6122fb6a4e39cd7b8afcfaab6772a66bdc7d5630
Sabrina Coelho <80803237+SabrinaCoelho@users.noreply.github.com>
2023-11-08 15:51:16 -0300

    liso

--- personal_projects/src/app/repositories/UserRepository.ts
+++ personal_projects/src/app/repositories/UserRepository.ts
@@ -7,22 +7,84 @@ const userRepository = AppDataSource.getRepository(User);
 const getUsers = async(_req: any, res: any): Promise<IUser[]> => {
     //return userRepository.find();
     const users = await AppDataSource.getRepository(User).find()
-    return res.status(200).json(users);
+    if(users && users.length){
+        return users;
+    }else{
+        throw new Error(JSON.stringify({
+            status: 500,
+            msg: "Erro ao buscar usuarios."
+            })
+        );
+        
+    }
 }
 
-const postUser = (u: User) => {
-    return userRepository.create(u);
+const postUser = async(newUser: User) => {
+    //return userRepository.create(u);
+    const user = await AppDataSource.getRepository(User).create(newUser)
+    const results = await AppDataSource.getRepository(User).save(user)
+    if(results){
+        return results;
+    }else{
+        throw new Error(JSON.stringify({
+            status: 500,
+            msg: "Erro ao criar usuario."
+            })
+        );
+    }
 }
 
-const getUserById = (user: User) => {
-    return userRepository.create(user);
+const getUserById = async(id: string) => {
+    //return userRepository.create(user);
+    
+    const results = await AppDataSource.getRepository(User).findOneBy({
+        id: parseInt(id),
+    })
+    if(results){
+        return results;
+    }else{
+        throw new Error(JSON.stringify({
+            status: 500,
+            msg: "Erro ao buscar usuario."
+            })
+        );
+    }
 }
 
-const updateUserById = (user: User) => {
-    return userRepository.create(user);
+const updateUserById = async(id: string, userNewData: User) => {
+    //return userRepository.create(user);
+    const userFounded = await AppDataSource.getRepository(User).findOneBy({
+        id: parseInt(id),
+    })
+    if(userFounded){
+        AppDataSource.getRepository(User).merge(userFounded, userNewData)
+        const results = await AppDataSource.getRepository(User).save(userFounded)
+        
+        if(results){
+            return results;
+        }else{
+            throw new Error(JSON.stringify({
+                status: 500,
+                msg: "Erro ao atualizar usuario."
+                })
+            );
+        }
+    }
 }
-const destroyUser = (user: User) => {
-    return userRepository.create(user);
+const destroyUser = async (id: string) => {
+    console.log(id);
+    //return userRepository.create(user);
+    const results = await AppDataSource.getRepository(User).delete(id)
+    console.log(results);
+    if(results){
+        return results;
+    }else{
+        throw new Error(JSON.stringify({
+            status: 500,
+            msg: "Erro ao deletar usuario."
+            })
+        );
+    }
 }
 
-export default { getUsers, postUser };
+export default { getUsers, postUser, getUserById, updateUserById, destroyUser};
